e9e416d83324e3f47435ff2302a27fc85ce49fb4
mohammadba86 <mohammadba86@yahoo.com>
2022-07-24 11:33:47 +0430

    Working with loops and other directives

--- FirstFolder/app.js
+++ FirstFolder/app.js
@@ -26,7 +26,33 @@
     angular.module('DIApp', [])
         .controller('DIController', DIController);
 
-    function DIController($scope) {
+    function DIController($scope, $filter) {
         $scope.name = "John";
-    }
+        $scope.upper = function () {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        }
+    };
+
+    angular.module('Index5', [])
+        .controller('Index5Controller', function ($scope) {
+            $scope.query = "";
+            $scope.artists = [{
+                "artistName": "John Smith",
+                "artistAge": "25",
+                "artistBio": "He was born in 1997 in usa , California ..."
+            }, {
+                "artistName": "Sara Callins",
+                "artistAge": "35",
+                "artistBio": "He was born in 1987 in usa , Los Angles ..."
+            }, {
+                "artistName": "James Rodriguez",
+                "artistAge": "21",
+                "artistBio": "He was born in 2001 in usa , Newyork ..."
+            }, {
+                "artistName": "Maria Brandi",
+                "artistAge": "32",
+                "artistBio": "He was born in 1990 in usa , Texas ..."
+            }]
+        });
 })();
